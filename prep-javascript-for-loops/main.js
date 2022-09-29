641bb881984b46098d6ba704ad56a3feea3df50b
Raymond Ho <raymondkhoaho@gmail.com>
2022-09-29 12:37:34 +0000

    Completed prep-javascript-for-loops exercise.

---
+++ prep-javascript-for-loops/main.js
@@ -0,0 +1,20 @@
+let count = ' ';
+
+for (let i = 0; i < 10; i++) {
+  count = i;
+  console.log(count);
+}
+
+let count2 = ' ';
+
+for (let i = 0; i < 10; i++) {
+  count2 = i * 2;
+  console.log(count2);
+}
+
+let count3 = ' ';
+
+for (let i = 100; i >= 0; i--) {
+  count3 = 'Time till explosion: ' + i + '!';
+  console.log(count3);
+}
